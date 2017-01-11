e59f89a24836ce8423e6eb442e29659d189f2ec8
somkiat <somkiat.p@gmail.com>
2017-01-11 13:59:17 +0700

    Add create database and table script

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ docker rm $(docker ps -a -q)
 docker build -t username/my-lamp-app .
 docker run --name parkko -d -p 80:80 -p 3306:3306 -e MYSQL_PASS="mypass" username/my-lamp-app
 sh waiting.sh
+sh mysql-setup.sh
